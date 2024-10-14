68ce332db3a3715112c087752ce635d9caa08637
Pieter-Jan de Vries <pieter@obix.nl>
2024-10-14 11:32:09 +0200

    Allows configurable custom field names for article lat/lon specification.

--- modules/mod_donkey_map/src/Helper/DonkeyMapHelper.php
+++ modules/mod_donkey_map/src/Helper/DonkeyMapHelper.php
@@ -14,6 +14,7 @@ namespace Joomla\Module\DonkeyMap\Site\Helper;
 use Joomla\CMS\Access\Access;
 use Joomla\CMS\Application\CMSApplicationInterface;
 use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Event\Content\ContentPrepareEvent;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Plugin\PluginHelper;
 use Joomla\CMS\Router\Route;
@@ -23,7 +24,6 @@ use Joomla\Component\Content\Site\Model\ArticlesModel;
 use Joomla\Component\Fields\Administrator\Helper\FieldsHelper;
 use Joomla\Database\DatabaseAwareInterface;
 use Joomla\Database\DatabaseAwareTrait;
-use Joomla\CMS\Event\Content\ContentPrepareEvent;
 use Joomla\Registry\Registry;
 use Joomla\Utilities\ArrayHelper;
 
@@ -47,7 +47,7 @@ class DonkeyMapHelper implements DatabaseAwareInterface
     private Registry $params;
 
     /**
-     * @param \Joomla\CMS\Application\SiteApplication $app
+     * @param   \Joomla\CMS\Application\SiteApplication  $app
      */
     public function __construct(array $config = [])
     {
@@ -59,8 +59,8 @@ class DonkeyMapHelper implements DatabaseAwareInterface
     /**
      * Returns an array containing marker definitions based on content of regular Joomla! articles.
      *
-     * @param \Joomla\Registry\Registry $params
-     * @param \Joomla\CMS\Application\SiteApplication $app
+     * @param   \Joomla\Registry\Registry                $params
+     * @param   \Joomla\CMS\Application\SiteApplication  $app
      *
      * @return array
      * @throws \Exception
@@ -70,11 +70,11 @@ class DonkeyMapHelper implements DatabaseAwareInterface
         $markers = [];
 
         // Convert category/marker associations to an array containing category config objects indexed by category id.
-        $selectedCategories = array_values((array)$this->params->get('categories', []));
+        $selectedCategories     = array_values((array)$this->params->get('categories', []));
         $selectedCategoriesById = array_reduce($selectedCategories, function (array $carry, object $category) {
             $carry[(int)$category->id[0]] = (object)[
-                'id' => $category->id[0],
-                'icon' => $category->icon ? Uri::root() . $category->icon : '',
+                'id'             => $category->id[0],
+                'icon'           => $category->icon ? Uri::root() . $category->icon : '',
                 'alternateTitle' => $category->alternate_title ?: '',
             ];
 
@@ -83,6 +83,16 @@ class DonkeyMapHelper implements DatabaseAwareInterface
 
         if ($prepareContent = (int)$this->params->get('prepare_content', 1)) {
             PluginHelper::importPlugin('content');
+        }
+
+        // The article is supposed to have one or more custom fields of type input[type=text],
+        // whose names can be specified in the module. If no field names are specified, a field
+        // named "location" is used by default. Location fields must contain a pair of
+        // numeric coordinates formatted as: lat,long, e.g.: 54.12995696954786,-2.4445791703280912.
+        if ($locationFieldNames = $this->params->get('location_field_names', null)) {
+            $locationFieldNames = array_map(fn(object $item) => $item->name, array_values((array)$locationFieldNames));
+        } else {
+            $locationFieldNames = ['location'];
         }
 
         // Process articles matching any filter setting as configured in the module instance
@@ -94,10 +104,7 @@ class DonkeyMapHelper implements DatabaseAwareInterface
             // Make custom field's accessible by name.
             $fieldsByName = ArrayHelper::pivot($article?->jcfields ?? [], 'name');
 
-            // The article is supposed to have a custom field named "location" of type input[type=text],
-            // which must contain a pair of numeric coordinates formatted as: lat,long.
-            // E.g.: 54.12995696954786,-2.4445791703280912
-            if (!isset($fieldsByName['location'])) {
+            if (!count($articleLocationFieldNames = array_filter($locationFieldNames, fn(string $name) => isset($fieldsByName[$name])))) {
                 continue;
             }
 
@@ -122,48 +129,51 @@ class DonkeyMapHelper implements DatabaseAwareInterface
                 );
             }
 
-            if (!($coordinates = $this->extractLatLon($fieldsByName['location']))) {
-                continue;
+            foreach ($articleLocationFieldNames as $locationFieldName) {
+                if (!($coordinates = $this->extractLatLon($fieldsByName[$locationFieldName]))) {
+                    continue;
+                }
+
+                // Split coordinates into separate latitude and longitude values.
+                // TODO: validate and handle invalid input.
+                [$lat, $long] = $coordinates;
+
+                // Extract and decode the article's image data.
+                $articleImages = json_decode($article->images);
+
+                // Compose marker popup content by combining article intro text and image.
+                $popupContent = $article->introtext;
+                if (!empty($articleImages->image_intro)) :
+                    $popupContent .= '<img src="' . Uri::root(
+                        ) . $articleImages->image_intro . '" style="width: 200px;">';
+                endif;
+
+                $markerTitle = count($selectedCategoriesById) && array_key_exists(
+                    (int)$article->catid,
+                    $selectedCategoriesById
+                )
+                    ? ($selectedCategoriesById[(int)$article->catid]->alternateTitle ?: $article->category_title)
+                    : $article->category_title;
+
+                // Accumulate marker data as objects in an array.
+                $markers[] = (object)[
+                    'id'          => (int)$article->id,
+                    'group'       => [
+                        'id'    => (int)$article->markerTypeId,
+                        'type'  => $article->markerType,
+                        'title' => $markerTitle,
+                    ],
+                    'coordinates' => (object)[
+                        'lat'  => (float)$lat,
+                        'long' => (float)$long,
+                    ],
+                    'title'       => $article->title,
+                    'popup'       => (object)[
+                        'content' => trim($popupContent),
+                        'link'    => trim($article->link),
+                    ],
+                ];
             }
-
-            // Split coordinates into separate latitude and longitude values.
-            // TODO: validate and handle invalid input.
-            [$lat, $long] = $coordinates;
-
-            // Extract and decode the article's image data.
-            $articleImages = json_decode($article->images);
-
-            // Compose marker popup content by combining article intro text and image.
-            $popupContent = $article->introtext;
-            if (!empty($articleImages->image_intro)) :
-                $popupContent .= '<img src="' . Uri::root() . $articleImages->image_intro . '" style="width: 200px;">';
-            endif;
-
-            $markerTitle = count($selectedCategoriesById) && array_key_exists(
-                (int)$article->catid,
-                $selectedCategoriesById
-            )
-                ? ($selectedCategoriesById[(int)$article->catid]->alternateTitle ?: $article->category_title)
-                : $article->category_title;
-
-            // Accumulate marker data as objects in an array.
-            $markers[] = (object)[
-                'id' => (int)$article->id,
-                'group' => [
-                    'id' => (int)$article->markerTypeId,
-                    'type' => $article->markerType,
-                    'title' => $markerTitle,
-                ],
-                'coordinates' => (object)[
-                    'lat' => (float)$lat,
-                    'long' => (float)$long,
-                ],
-                'title' => $article->title,
-                'popup' => (object)[
-                    'content' => trim($popupContent),
-                    'link' => trim($article->link),
-                ],
-            ];
         }
 
         $a[] = array_map(fn(object $a) => ['id' => $a->id, 'type' => $a->group['type']], $markers);
@@ -191,8 +201,6 @@ class DonkeyMapHelper implements DatabaseAwareInterface
         $a[] = array_map(fn(object $a) => ['id' => $a->id, 'type' => $a->group['type']], $markers);
 
         $markers = array_values($markers);
-
-        $a[] = array_map(fn(object $a) => ['id' => $a->id, 'type' => $a->group['type']], $markers);
 
         return $markers;
     }
@@ -272,7 +280,7 @@ class DonkeyMapHelper implements DatabaseAwareInterface
             foreach ($this->getFilteredArticles($model, ['filter.category_id' => $markerCategoryIds]) as $article) {
                 $article = (array)$article;
 
-                $article['markerType'] = 'category';
+                $article['markerType']   = 'category';
                 $article['markerTypeId'] = (int)$article['catid'];
 
                 yield (object)$article;
@@ -290,7 +298,7 @@ class DonkeyMapHelper implements DatabaseAwareInterface
 
             foreach ($this->getFilteredArticles($model, ['filter.tag' => $markerTagIds]) as $article) {
                 $articleTagIds = array_map(fn(object $tag) => (int)$tag->id, $article->tags->itemTags);
-                $tagIds = array_values(array_intersect($markerTagIds, $articleTagIds));
+                $tagIds        = array_values(array_intersect($markerTagIds, $articleTagIds));
 
                 $article = (array)$article;
 
@@ -306,7 +314,7 @@ class DonkeyMapHelper implements DatabaseAwareInterface
     /**
      * Retrieve a list of article, filtered by attributes passed in the argument.
      *
-     * @param array $filters
+     * @param   array  $filters
      *
      * @return \Generator
      */
@@ -319,7 +327,7 @@ class DonkeyMapHelper implements DatabaseAwareInterface
         $user = $this->app->getIdentity();
 
         // Authotisation and acces permission.
-        $access = !ComponentHelper::getParams('com_content')->get('show_noauth');
+        $access     = !ComponentHelper::getParams('com_content')->get('show_noauth');
         $authorised = Access::getAuthorisedViewLevels($user->id);
 
         foreach ($model->getItems() as $item) {
